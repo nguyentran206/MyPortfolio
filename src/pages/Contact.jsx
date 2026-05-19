4b184891ec67d6da88c52fa2259850e3e2fc5f3c
Tran Thi Thao Nguyen <thaonguyen01122024@gmail.com>
2026-05-19 15:34:18 +0700

    Update contact infor

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,13 +17,13 @@ export default function ContactSection() {
       </p>
 
       <ul className="contact-list">
-        <li className="email"><MdEmail /> <strong>Email:</strong> <a href="mailto:thaonguyen01122024@gmail.com">thaonguyen01122024@gmail.com</a></li>
+        <li className="email"><MdEmail /> <strong>Email:</strong> <a href="mailto:tnguyentran.work@gmail.com">thaonguyen01122024@gmail.com</a></li>
         <li className="phone"><FaPhone /> <strong>Phone:</strong> <a href="tel:+84354151057">+84 354 151 057</a></li>
         <li className="facebook"><FaFacebook /> <strong>Facebook:</strong>{" "}<a href="https://www.facebook.com/thaonguyencktv" target="_blank" rel="noreferrer">facebook.com/thaonguyencktv</a></li>
         <li className="zalo"><SiZalo /> <strong>Zalo:</strong> <a href="https://zalo.me/0354151057" target="_blank" rel="noreferrer">zalo.me/0354151057</a></li>
         <li className="linkedin"><FaLinkedin /> <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/thao-nguyen-tran-441a78320/" target="_blank" rel="noreferrer">linkedin.com/in/thao-nguyen-tran-441a78320</a></li>
-        <li className="github"><FaGithub /> <strong>GitHub:</strong> <a href="https://github.com/thaonguyen20604" target="_blank" rel="noreferrer">github.com/thaonguyen20604</a></li>
-        <li className="huggingface"><SiHuggingface /> <strong>HuggingFace:</strong>{" "}<a href="https://huggingface.co/tnguyen20604" target="_blank" rel="noreferrer">huggingface.co/tnguyen20604</a></li>
+        <li className="github"><FaGithub /> <strong>GitHub:</strong> <a href="https://github.com/nguyentran206" target="_blank" rel="noreferrer">github.com/thaonguyen20604</a></li>
+        <li className="huggingface"><SiHuggingface /> <strong>HuggingFace:</strong>{" "}<a href="https://huggingface.co/nguyentran206" target="_blank" rel="noreferrer">huggingface.co/tnguyen20604</a></li>
         <li className="kaggle"><SiKaggle /> <strong>Kaggle:</strong>{" "}<a href="https://www.kaggle.com/nguyentran20604" target="_blank" rel="noreferrer">kaggle.com/nguyentran20604</a></li>
       </ul>
 
